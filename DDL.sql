354124e1749c5e00e7e38347932218596983a163
Alan Munoz Martins <63621071+alanmmartins@users.noreply.github.com>
2023-10-30 21:44:07 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,70 +1,74 @@
-CREATE DATABASE bd_volksway;
+CREATE DATABASE db_volksway;
 
-USE bd_volksway;
+USE db_volksway;
 
-CREATE TABLE tb_usuarios(
+CREATE TABLE tb_empresa(
+	id_empresa BINARY(16) NOT NULL,
+    razao_social VARCHAR(50) NOT NULL UNIQUE,
+	cidade VARCHAR(50) NOT NULL,
+    cnpj CHAR(18) NOT NULL UNIQUE,
+    PRIMARY KEY(id_empresa)
+);
+
+CREATE TABLE tb_usuario(
 	id_usuario BINARY(16) NOT NULL,
-    id_empresa BINARY(16) NOT NULL,
-    nome VARCHAR(50) NOT NULL UNIQUE,
-    telefone CHAR(50) NOT NULL UNIQUE,
+    id_empresa	BINARY(16) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    telefone CHAR(15) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(50) NOT NULL UNIQUE,
-    dataNascimento DATE NOT NULL UNIQUE,
-    cidade VARCHAR(50) NOT NULL UNIQUE,
-    cpf CHAR(50) NOT NULL UNIQUE,
-    tipoUsuario TINYINT NOT NULL UNIQUE,
-    
-    PRIMARY KEY(id_usuario)
-    );
-    
-DROP TABLE tb_usuarios;
+    data_nascimento DATE NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    cpf CHAR(14) NOT NULL UNIQUE,
+    tipo_usuario TINYINT NOT NULL,
+    PRIMARY KEY (id_usuario),
+    FOREIGN KEY(id_empresa) REFERENCES tb_empresa(id_empresa)
+);
 
-    
-CREATE TABLE tb_empresas(
-	id_empresa BINARY(16) NOT NULL,
-	rasaoSocial VARCHAR(50) NOT NULL UNIQUE,
-    cidade VARCHAR(50) NOT NULL UNIQUE,
-    cnpj CHAR(50) NOT NULL UNIQUE
-	);
-    
-DROP TABLE tb_empresas;
-    
-CREATE TABLE tb_propagandas(
-	id_propaganda BINARY(16) NOT NULL,
-	titulo VARCHAR(50) NOT NULL UNIQUE,
-	url VARCHAR(50) NOT NULL UNIQUE,
-    descricao VARCHAR(50) NOT NULL UNIQUE,
-    imagem VARCHAR(50) NOT NULL UNIQUE,
-	publicoAlvo VARCHAR(50) NOT NULL UNIQUE,
-    dataLimite DATE,
-    preco FLOAT,
-    importancia BIT
-	);
-    
-DROP TABLE tb_propagandas;
-        
-CREATE TABLE tb_veicuolos(
+CREATE TABLE tb_veiculo(
 	id_veiculo BINARY(16) NOT NULL,
     id_usuario BINARY(16) NOT NULL,
-	codigoChassi CHAR(50) NOT NULL UNIQUE,
-    marca CHAR(50) NOT NULL UNIQUE,
-    placa CHAR(8) NOT NULL UNIQUE
-);  
-  
-CREATE TABLE tb_interesses(
-	id_interesse BINARY(16) NOT NULL,
-    id_propaganda BINARY(16) NOT NULL,
-    id_usuario BINARY(16) NOT NULL,
-    meioContatoEmail BIT
-);  
+    marca VARCHAR(30) NOT NULL,
+	placa CHAR(8) NOT NULL UNIQUE,
+    codigo_chassi CHAR(20) NOT NULL UNIQUE,
+    PRIMARY KEY (id_veiculo),
+    FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id_usuario)
+);
 
 CREATE TABLE tb_checklist(
 	id_checklist BINARY(16) NOT NULL,
     id_veiculo BINARY(16) NOT NULL,
-    freio BIT,
-    combultivel BIT,
-    oleo BIT,
-    fotoPneu VARCHAR(50) NOT NULL UNIQUE,
-    arCondicionado BIT
-    
+    freio BIT NOT NULL,
+    combustivel BIT NOT NULL,
+    oleo BIT NOT NULL,
+    ar_condicionado BIT NOT NULL,
+    foto_pneu VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id_checklist),
+    FOREIGN KEY(id_veiculo) REFERENCES tb_veiculo(id_veiculo)
+);
+
+CREATE TABLE tb_propaganda(
+	id_propaganda BINARY(16) NOT NULL,
+    id_usuario BINARY(16) NOT NULL,
+    titulo VARCHAR(50) NOT NULL,
+    url VARCHAR(256) NOT NULL,
+    descricao VARCHAR(256) NOT NULL,
+	img VARCHAR(256) NOT NULL,
+    publico_alvo VARCHAR(30) NOT NULL,
+	data_limite DATE NOT NULL,
+    preco FLOAT(10,2) NOT NULL,
+    importancia BIT NOT NULL,
+    nomeTipoPropaganda VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id_propaganda),
+    FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id_usuario)
+);
+
+CREATE TABLE tb_interesse(
+	id_interesse BINARY(16) NOT NULL,
+    id_propaganda BINARY(16) NOT NULL,
+    id_usuario BINARY(16) NOT NULL,
+    meio_contato_email BIT NOT NULL,
+    PRIMARY KEY (id_interesse),
+    FOREIGN KEY(id_propaganda) REFERENCES tb_propaganda(id_propaganda),
+    FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id_usuario)
 );
